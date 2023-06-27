8a381b830fe5bf38620d9145b598fa4f7d742847
tiankuo <tiankuo1233@163.com>
2023-06-28 00:25:54 +0800

    STL六大部件的简单测试

--- STL/Test_1.cpp
+++ STL/Test_1.cpp
@@ -8,6 +8,10 @@ using namespace std;
 int main()
 {
     int ia[ 6 ] = { 27, 210, 12, 47, 109, 83 };
+    for(int i : ia){
+        cout << i << " " ;
+    }
+    cout << endl;
     // vector表示容器<itemtype, 分配器>
     vector<int, allocator<int>> vi(ia,ia + 6);
     // count_if表示函数
